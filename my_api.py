00512e8ed74c6d401c826d695db31fb8be54eb75
Hrishikesh-2712 <74128509+Hrishikesh-2712@users.noreply.github.com>
2022-01-11 22:37:01 +0530

    Update my_api.py

--- my_api.py
+++ my_api.py
@@ -9,9 +9,10 @@ app=Flask(__name__)
 def handle_request():
     text=str(request.args.get('input')) #requires ?input=a
     print(text)
-    d={'/logbook?input=(player_id)':'get player monthly data','/company-employee?input=(compamy_id)':'get company data like members,position,player_id'}
-    json_dump= json.dumps(d)
-    return json_dump
+    #d={'/logbook?input=(player_id)':'get player monthly data','/company-employee?input=(compamy_id)':'get company data like members,position,player_id'}
+    #json_dump= json.dumps(d)
+    #return json_dump
+    return "<h1>Welcome</h1>"
 
 @app.route('/logbook', methods=['GET','POST'])
 def logbook():
@@ -30,6 +31,6 @@ def company_employee():
     return json_dump
 if __name__ == '__main__':
     try:
-        app.run(debug=True)
+        app.run()
     except Exception as e:
         print(e)
