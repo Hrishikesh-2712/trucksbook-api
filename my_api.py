a83cb29484ef2319004544a701da6b76436c61cd
Hrishikesh-2712 <74128509+Hrishikesh-2712@users.noreply.github.com>
2022-01-12 14:05:35 +0530

    Update my_api.py

--- my_api.py
+++ my_api.py
@@ -2,25 +2,39 @@ from flask import Flask
 from flask import request
 import json
 import trucksbook
-
+from datetime import datetime
 app=Flask(__name__)
 
-@app.route('/')
-def home_view():
-    d={'/logbook?input=(player_id)':'get player monthly data','/company?input=(compamy_id)':'get company data like members,position,player_id','author':'Ben X 🎄#8003'}
+@app.route('/', methods=['GET','POST'])
+def handle_request():
+    text=str(request.args.get('input')) #requires ?input=a
+    print(text)
+    d={'/logbook?id=(player_id)?year=(year)?month=(month)':'get player monthly data','/company?id=(company_id)':'get company data like members,position,player_id'}
     json_dump= json.dumps(d)
     return json_dump
 
 @app.route('/logbook', methods=['GET','POST'])
 def logbook():
-    text=str(request.args.get('input')) #requires ?input=a
-    print(text)
-    d=trucksbook.logb(text)
+    args=[]
+    id_  = request.args.get('id', None)
+    year  = request.args.get('year', datetime.now().strftime('%Y'))
+    month = request.args.get('month', datetime.now().strftime('%m'))
+    #text=str(request.args.get('input')) #requires ?input=a
+    arg=''
+    if id_:
+        arg+='/'+id_
+    if year:
+        arg+='/'+year
+    if month:
+        arg+='/'+month
+    print(id_,year,month)
+    print(arg)
+    d=trucksbook.logb(arg)
     json_dump= json.dumps(d)
     return json_dump
 
 @app.route('/company', methods=['GET','POST'])
-def company():
+def company_members():
     text=str(request.args.get('input')) #requires ?input=a
     print(text)
     d=trucksbook.company(text)
@@ -28,6 +42,6 @@ def company():
     return json_dump
 if __name__ == '__main__':
     try:
-        app.run(debug=True)
+        app.run(debug=True, host='0.0.0.0')
     except Exception as e:
         print(e)
