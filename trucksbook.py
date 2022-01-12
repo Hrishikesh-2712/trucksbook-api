053579ec504585f7e033d050b47cc0c84406bdc4
Hrishikesh-2712 <74128509+Hrishikesh-2712@users.noreply.github.com>
2022-01-12 14:04:13 +0530

    Update trucksbook.py

--- trucksbook.py
+++ trucksbook.py
@@ -1,138 +1,47 @@
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask
+from flask import request
 import json
-import re
-#from requests.packages.urllib3.exceptions import InsecureRequestWarning
-#requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+import trucksbook
+from datetime import datetime
+app=Flask(__name__)
 
-url='https://trucksbook.eu/components/notlogged/login.php'
-with open('creds.json','r') as j:
-    values=json.load(j)
+@app.route('/', methods=['GET','POST'])
+def handle_request():
+    text=str(request.args.get('input')) #requires ?input=a
+    print(text)
+    d={'/logbook?id=(player_id)?year=(year)?month=(month)':'get player monthly data','/company?id=(company_id)':'get company data like members,position,player_id'}
+    json_dump= json.dumps(d)
+    return json_dump
 
-#excell express company_id=144260
+@app.route('/logbook', methods=['GET','POST'])
+def logbook():
+    args=[]
+    id_  = request.args.get('id', None)
+    year  = request.args.get('year', datetime.now().strftime('%Y'))
+    month = request.args.get('month', datetime.now().strftime('%m'))
+    #text=str(request.args.get('input')) #requires ?input=a
+    arg=''
+    if id_:
+        arg+='/'+id_
+    if year:
+        arg+='/'+year
+    if month:
+        arg+='/'+month
+    print(id_,year,month)
+    print(arg)
+    d=trucksbook.logb(arg)
+    json_dump= json.dumps(d)
+    return json_dump
 
-resp = requests.post(url, data=values)
-if resp.status_code==200 and 'success' in str(resp.content):
-    print(resp.content)
-    with requests.Session() as sess:
-        res=sess.post(url, data=values)
-    def logb(playerid):
-        #logbook
-        res=sess.get(f'https://trucksbook.eu/logbook/{playerid}')
-        if res.status_code==200:
-            pass
-        else:
-            return 'error'
-        soup=BeautifulSoup(res.text,'lxml')
-        #player_monthly_data
-        span=soup.find_all('div', class_='value')
-        logbook=dict()
-        def dd(f):
-            digit=''
-            f=''.join(f.split())
-            for i in range(len(f)):
-                if f[i].isdigit():
-                    digit+=f[i]
-            return digit
-
-        def game(g):
-            # euro or dollar
-            if '€' in g:
-                return 'Euro Truck Simulator 2'
-            elif '$' in g:
-                return 'American Truck Simulator'
-            else:
-                return 'Null'
-        if len(span)==0:
-            #game data
-            logbook={'Euro Truck Simulator 2':{'Profit':'0',
-                     'Driven Distance':'0',
-                     'XP':'0',
-                     'Fuel Cost':'0',
-                     'Offences':'0',
-                     'Cargo weight':'0'},
-                     'American Truck Simulator':{'Profit':'0',
-                     'Driven Distance':'0',
-                     'XP':'0',
-                     'Fuel Cost':'0',
-                     'Offences':'0',
-                     'Cargo weight':'0'}
-                    }
-            return {soup.find_all('div',class_='nick-card')[0].text:logbook}
-        elif len(span)==6:
-            #game data
-            g=game(span[0].text)
-            if 'Euro Truck Simulator 2' in g:
-                g2='American Truck Simulator'
-            elif 'American Truck Simulator' in g:
-                g2='Euro Truck Simulator 2'
-            else:
-                g2='Null'
-            
-            logbook={game(span[0].text):{'Profit':dd(span[0].text),
-                     'Driven Distance':dd(span[1].text),
-                     'XP':dd(span[2].text),
-                     'Fuel Cost':dd(span[3].text),
-                     'Offences':dd(span[4].text),
-                     'Cargo weight':dd(span[5].text)},
-                     g2:{'Profit':'0',
-                     'Driven Distance':'0',
-                     'XP':'0',
-                     'Fuel Cost':'0',
-                     'Offences':'0',
-                     'Cargo weight':'0'}}
-            return {soup.find_all('div',class_='nick-card')[0].text:logbook}
-            
-        elif len(span)==12:
-            logbook={game(span[0].text):{'Profit':dd(span[0].text),
-                     'Driven Distance':dd(span[1].text),
-                     'XP':dd(span[2].text),
-                     'Fuel Cost':dd(span[3].text),
-                     'Offences':dd(span[4].text),
-                     'Cargo weight':dd(span[5].text)},
-                     game(span[6].text):{'Profit':dd(span[6].text),
-                     'Driven Distance':dd(span[7].text),
-                     'XP':dd(span[8].text),
-                     'Fuel Cost':dd(span[9].text),
-                     'Offences':dd(span[10].text),
-                     'Cargo weight':dd(span[11].text)}}
-            return {soup.find_all('div',class_='nick-card')[0].text:logbook}
-        else:
-            pass
-    def company(company_id):
-        #returns name, position, trucksbook_player_id
-        i=-1
-        bd=''
-        data={}
-        members={}
-        res=sess.get(f'https://trucksbook.eu/company/{company_id}')
-        soup=BeautifulSoup(res.text,'lxml')
-        company_name=soup.find_all('h4')[0].text.strip()
-        founder=soup.find_all('div',class_='left')[0].text.strip()
-        founder_id=soup.findAll('a', attrs={'href': re.compile("^https://trucksbook.eu/profile/")})[1].get('href').split('/')[-1]
-        for s in soup.find_all('div',class_='profile-info-item'):
-            b=''.join(s.text.strip().split('.'))
-            if b.isdigit():
-                bd=b
-                break
-        try:
-            if 'The company was dissolved!' in soup.find_all('div',class_='alert alert-danger')[0].text:
-                status='Closed'
-                members['']=''
-                i=-1
-            else:
-                pass
-        except IndexError:
-            status='Active'
-            res=sess.get(f'https://trucksbook.eu/components/app/company/employee_list.php?id={company_id}')
-            soup=BeautifulSoup(res.text,'lxml')
-            players=soup.find_all('a', href=True, text=True)    
-            positions=soup.find_all('td',class_="d-none d-sm-table-cell text-center")
-            player_id=soup.findAll('a', attrs={'href': re.compile("^https://")})
-            for i in range(len(players)):
-                pl=''.join(players[i].text.split())
-                pos=positions[i].text
-                pid=player_id[i].get('href').split('/')[-1]
-                members[pl]={'position':pos,'player_id':pid}
-        data={'status':status,'company':{'name':company_name,'founder':founder,'founder_id':founder_id,'born':bd,'employee_count':i+1,'employees':members}}
-        return data
+@app.route('/company', methods=['GET','POST'])
+def company_members():
+    text=str(request.args.get('input')) #requires ?input=a
+    print(text)
+    d=trucksbook.company(text)
+    json_dump= json.dumps(d)
+    return json_dump
+if __name__ == '__main__':
+    try:
+        app.run(debug=True, host='0.0.0.0')
+    except Exception as e:
+        print(e)
